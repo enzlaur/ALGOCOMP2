9807244b107906c18e027209f8297bc55f9bab52
laurenz <laurenztino@yahoo.com>
2017-03-25 03:03:41 +0800

    yeeeee boiiiiii

--- TestHere/cluster.py
+++ TestHere/cluster.py
@@ -3,7 +3,8 @@
 
 import csv
 import numpy as np
-
+from numpy.random import rand
+import matplotlib.pyplot as plt
 
 from sklearn.cluster import KMeans
 
@@ -27,11 +28,47 @@ def usewith():
     for i in range(len(points)):
         point = points[i]
         label = kmeans.labels_[i]
-        clusterpoints.append([[float(point[0]), float(point[1]), label]])
+        clusterpoints.append([float(point[0]), float(point[1]), label])
     # print(kmeans.labels_)
     # print(kmeans.cluster_centers_)
-    print(clusterpoints)
+    return clusterpoints
+
+
+def colorchoices(x):
+    return {
+        0: 'red',
+        1: 'blue',
+        2: 'orange'
+    }[x]
+
+
+def createscatter():
+    clusterpoints = usewith()
+    fig, ax = plt.subplots()
+    print("len: ", len(clusterpoints))
+    for i in range(len(clusterpoints)):
+        point = clusterpoints[i]
+        x = point[0]
+        y = point[1]
+        z = point[2]
+        color = colorchoices(z)
+        ax.scatter(x, y, c=color)
+    ax.grid(True)
+    plt.show()
+
+
+def testmatplot():
+    fig, ax = plt.subplots()
+    for color in ['red', 'green', 'blue']:
+        n = 100
+        x, y = rand(2, n)
+        scale = 200.0 * rand(n)
+        ax.scatter(x, y, c=color, s=scale, label=color,alpha=0.3, edgecolors='none')
+    ax.legend()
+    ax.grid(True)
+    plt.show()
 
 
 # Run your crap below
-usewith()
+# createscatter()
+createscatter()
